aad95c89ba008c932dc41016c40a08d290258bf9
priyanka-sajan <priyanka.ms@techversantinfo.com>
2022-04-05 14:32:41 +0530

    updated

--- formvalidation/js/form.js
+++ formvalidation/js/form.js
@@ -21,19 +21,19 @@ function validate()
     let file=document.getElementById("file").value;
     let errors=document.getElementsByClassName("errors");
     let fail=document.getElementsByClassName("fa-circle-exclamation");
-    let validPhone=/\d{10}$/;
+    let validPhone=/^\d{10}$/;
     let validEmail=/[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+[.][a-zA-Z0-9+_.-]+/;
     let name=/[!@#$%^&*]/;
      
         //  firstname validation
     if(firstname == "") 
         {
-            errors[0].innerHTML="please enter the name";
+            errors[0].innerHTML="Please enter the name";
             fail[0].style.visibility="visible";
         }
         else if(!isNaN(firstname))
         {
-            errors[0].innerHTML="name should be in character";
+            errors[0].innerHTML="Name should be in character";
             fail[0].style.visibility="visible";
             
         }
@@ -50,13 +50,13 @@ function validate()
         // lastname validation
     if(lastname == "") 
         {
-            errors[1].innerHTML="enter the last name";
+            errors[1].innerHTML="Enter the last name";
             fail[1].style.visibility="visible";
            
         }
         else if(!isNaN(lastname))
         {
-            errors[1].innerHTML="name should be in character";
+            errors[1].innerHTML="Name should be in character";
             fail[1].style.visibility="visible";
             
         }
@@ -73,13 +73,13 @@ function validate()
         // Email validation
     if(mail=="")
         {
-            errors[2].innerHTML="enter email";
+            errors[2].innerHTML="Enter email";
             fail[2].style.visibility="visible";
            
         }
         else if (!mail.match(validEmail))
         {
-           errors[2].innerHTML="enter a valid email";
+           errors[2].innerHTML="Enter a valid email";
            fail[2].style.visibility="visible";
         }
         else
@@ -91,12 +91,12 @@ function validate()
         // password validation
     if(password == "")
         {
-            errors[3].innerHTML="enter the Password";
+            errors[3].innerHTML="Enter the Password";
             fail[3].style.visibility="visible";
         }
         else if(password.length<8)
         {
-            errors[3].innerHTML="enter atleast 8 characters";
+            errors[3].innerHTML="Enter atleast 8 characters";
             fail[3].style.visibility="visible";
         }
         else
@@ -107,7 +107,7 @@ function validate()
         //confirm-password Validdation
     if(cpassword!=password)
         {
-            errors[4].innerHTML="password doesn't match";
+            errors[4].innerHTML="Password doesn't match";
             fail[4].style.visibility="visible";
         }
         else
@@ -118,7 +118,7 @@ function validate()
         // gender validation
     if(gen[0].checked==false && gen[1].checked==false && gen[2].checked==false)
         {
-         errors[5].innerHTML="select the gender";
+         errors[5].innerHTML="Select the gender";
          fail[5].style.visibility="visible";
         }
      else{
@@ -128,7 +128,7 @@ function validate()
         // dob validation
     if (dateofbirth == "")
         {
-            errors[6].innerHTML="enter the date";
+            errors[6].innerHTML="Enter the date";
             fail[6].style.visibility="visible";
         }
         else
@@ -139,12 +139,12 @@ function validate()
         // phone no validation
     if(phone=="")
         {
-            errors[7].innerHTML="enter phone no:";
+            errors[7].innerHTML="Enter phone no:";
             fail[7].style.visibility="visible";
         }
         else if(!phone.match(validPhone))
         {
-            errors[7].innerHTML="enter a valid number";
+            errors[7].innerHTML="Enter a valid number";
             fail[7].style.visibility="visible";
         }
         else
@@ -155,7 +155,7 @@ function validate()
         // message validation
     if(description.length<10)
        {
-           errors[8].innerHTML="message feild cannot be empty";
+           errors[8].innerHTML="Message feild cannot be empty";
            fail[8].style.visibility="visible";
        }
        else
@@ -166,7 +166,7 @@ function validate()
         // language validation
     if(lang1==false && lang2==false && lang3==false)
         {
-            errors[9].innerHTML="select a language";
+            errors[9].innerHTML="Select a language";
             fail[9].style.visibility="visible";
          }
         else
@@ -177,7 +177,7 @@ function validate()
         //  file validation
     if(file=="") 
         {
-            errors[10].innerHTML="choose a file";
+            errors[10].innerHTML="Choose a file";
             fail[10].style.visibility="visible";
         }
         else
