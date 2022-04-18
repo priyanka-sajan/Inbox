fcc8076c8def560bbb1dbb3b0c6a181abb0940c0
priyanka-sajan <priyanka.ms@techversantinfo.com>
2022-04-18 18:53:07 +0530

    updated

--- Tab/js/tab.js
+++ Tab/js/tab.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$(do$(".tabNav li").click(function(){
 		var tabid = $(this).find("a").attr("href");
-		$(".tabNav li,.tabs div.tab").removeClass("active");   
+		$(".tabNav li,.main-container div.tab").removeClass("active");   
 		$(".tab").hide();   
 		$(tabid).show();    
 		$(this).addClass("active"); 
