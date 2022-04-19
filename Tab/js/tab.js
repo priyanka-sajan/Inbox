852b6ab2ccc8a4958e39a1414041ddb103b84dd1
priyanka-sajan <priyanka.ms@techversantinfo.com>
2022-04-19 09:25:00 +0530

    updated

--- Tab/js/tab.js
+++ Tab/js/tab.js
@@ -1,9 +1,25 @@
-$(document).ready(function(){
-	$(do$(".tabNav li").click(function(){
-		var tabid = $(this).find("a").attr("href");
-		$(".tabNav li,.main-container div.tab").removeClass("active");   
-		$(".tab").hide();   
-		$(tabid).show();    
-		$(this).addClass("active"); 
-	});		
-}); 	
+$(document).ready(function () {
+  $('.link').click(function () {
+    $('.link').removeClass('active');
+    $(this).addClass('active');
+    tab = $(this).attr('href');
+    $('.tabs .active').removeClass('active');
+    $(tab).addClass('active');
+  });
+});
+
+
+
+
+
+
+
+// $(document).ready(function(){
+// 	$(do$(".tabNav li").click(function(){
+// 		var tabid = $(this).find("a").attr("href");
+// 		$(".tabNav li,.main-container div.tab").removeClass("active");   
+// 		$(".tab").hide();   
+// 		$(tabid).show();    
+// 		$(this).addClass("active"); 
+// 	});		
+// }); 	
---
+++ modal/js/modal.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".open-btn").click(function () {
+      $(".content").addClass("show");
+  })
+})
+
+
+
+
+
+
