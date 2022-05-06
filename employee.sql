978898ac514ae0a17a984ebeb0848f5124016761
priyanka ms <priyanka.ms@techversantinfo.com>
2022-05-06 14:27:24 +0530

    updated

--- employee.sql
+++ employee.sql
@@ -1,25 +1,25 @@
 create table employees (
-emp_id integer not null,
+emp_id integer primary key not null,
 emp_name varchar(15),
 job_name varchar(10),
 manager_id integer,
 hire_date date,
 salary decimal(10,2),
 commisssion decimal(7,2),
-dept_id integer
+dep_id integer
 );
 create table salary_grade(
-grade integer,
+grade integer primary key,
 min_salary integer,
 max_salary integer
 );
 create table department(
-dept_id integer,
-dept_name varchar(20),
-dept_location varchar(15)
+dep_id integer primary key,
+dep_name varchar(20),
+dep_location varchar(15)
 );
 insert into employees (emp_id,emp_name,job_name,manager_id,
-hire_date,salary,commisssion,dept_id)
+hire_date,salary,commisssion,dep_id)
 values (68319,"KAYLING","PRESIDENT",null,"1991-11-18",6000.00,null,1001),
 (66928,"BLAZE","MANAGER",68319,"1991-05-01",2750.00,null,3001),
 (67832,"CLARE","MANAGER",68319,"1991-06-09",2550.00,null,1001),
@@ -40,21 +40,23 @@ values(1,800,1300),
 (3,1501,2100),
 (4,2101,3100),
 (5,3101,9999);
-insert into department(dept_id,dept_name,dept_location)
+insert into department(dep_id,dep_name,dep_location)
 values(1001,"FINANCE","SYDNEY"),
 (2001,"AUDIT","MELBOURINE"),
 (3001,"MARKETING","PERTH"),
 (4001,"PRODUCTION","BRISBANE");
 select * from employees;
+select * from salary_grade;
+select * from department;
 select salary from employees;
 select distinct job_name from employees;
 select emp_name,
 concat('$',salary*15/100+salary) as salary from employees;
 select concat(emp_name,"    ",job_name) as "Employee & Job" from employees; 
-select emp_id,emp_name,salary,date_format(hire_date,'%M %d %Y') from employees;
+select emp_id,emp_name,salary,date_format(hire_date,'%M %d, %Y') from employees;
 select length(trim(emp_name)) from employees;
-select distinct dept_id,job_name from employees;
-select * from employees where dept_id not in (2001);
+select distinct dep_id,job_name from employees;
+select * from employees where dep_id not in (2001);
 select * from employees where hire_date<("1991-1-1");
 select avg(salary) from employees where job_name="ANALYST";
 select * from employees where emp_name="BLAZE";
@@ -66,3 +68,10 @@ select * from employees where job_name not in("president","manager") order by sa
 select max(salary) from employees;
 select job_name,avg(salary),avg(salary+commisssion) from employees group by job_name;
 select * from employees where emp_name like '%AR%';
+select employees.emp_id,employees.emp_name,department.dep_id,department.dep_location,department.dep_name
+from employees,department
+where department.dep_id=employees.dep_id && employees.dep_id in(1001,2001);
+select manager_id,count(*) as count from employees 
+group by manager_id order by manager_id asc;
+select dep_id,count(*) as count from employees
+group by dep_id having count(*)>=2;
